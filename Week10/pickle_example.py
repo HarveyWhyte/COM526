88e655585e4e48535c5d3e42fe05c87c9c4dc6f2
Harvey <harveywhyte@outlook.com>
2026-03-25 10:51:14 +0000

    best model

--- Week10/pickle_example.py
+++ Week10/pickle_example.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 import pickle
+from sklearn.model_selection import GridSearchCV, KFold
 
 data = pd.read_csv("Week9/titanic_new.csv")
 
@@ -12,13 +13,28 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
 dt = DecisionTreeClassifier()
-dt_model = dt.fit(X_train, y_train)
 
-dt_pred = dt_model.predict(X_test)
+# get the best model
+# Hyperparameters and the values to try
+param_grid = {
+    'max_depth': [None, 2, 4, 6],
+    'min_samples_split': [2, 5, 10],
+}
 
+# KFold cross validation with 5 folds
+kf = KFold(n_splits=5, shuffle=True)
+
+# Create your grid search and train the models
+grid_search = GridSearchCV(dt, param_grid, cv=kf, scoring="accuracy")
+grid_search.fit(X, y)
+best_model = grid_search.best_estimator_
+
+
+#save best model
 #with open("Week10/titanic_model.pkl", "wb") as file:
-#    pickle.dump(dt_model, file)
+#    pickle.dump(best_model, file)
 
+#load
 with open("Week10/titanic_model.pkl", "rb") as file:
     t_model = pickle.load(file)
 
